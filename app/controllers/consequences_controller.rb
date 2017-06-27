e77dc20632e7a1b54ab9c64ad22d1b09d0b28228
blake <blake.rego@gmail.com>
2017-06-27 19:31:57 -0400

    Obtaining a page access token from the user graph.  In the immediate term, just using the first page that a given user can manage. In the future, we may need to distinguish one of many user pages (which may need to be persisted on the FacebookConfig object). However, for now, we will use the assume the first page is the correct one.

--- app/controllers/consequences_controller.rb
+++ app/controllers/consequences_controller.rb
@@ -1,7 +1,20 @@
 class ConsequencesController < ApplicationController
 
-  def createOffer
-    svc = BjondService.find_by_group_id(params[:groupid])
+  skip_before_filter :verify_authenticity_token, :only => [:create_offer]
+
+  def create_offer
+    registration = BjondRegistration.find_registration_by_remote_request(request)
+    fb_config = FacebookConfig.find_by_bjond_registration_id_and_group_id(registration.id, params['groupid'])
+    user_graph = Koala::Facebook::API.new(fb_config.user_app_token)
+    pages = user_graph.get_connections('me', 'accounts')
+    
+    ## For simplicity, just using first page found for user. If the user manages multiple pages, 
+    ## we may need to store the page ID in the FacebookConfig object.
+    page_access_token = pages[0]["access_token"]
+    page_graph = Koala::Facebook::API.new(page_access_token)
+    page_graph.get_connection('me')
+    # binding.pry
     puts 'Received consequence callback form Bjond server'
+    render :json => {:status => 'ok'}
   end
 end
